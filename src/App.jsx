2dd639fe1eeca922ab13599b83e7f4dfa8063a90
Karina Muñoz <karina.m.soto@gmail.com>
2025-03-10 00:15:06 -0300

    Modificación navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Logo from './components/Logo/Logo';
 import CartWidget from './components/CartWidget/CartWidget.jsx';
 
 
 function App() {
   return (
     <>
-    <Logo/>
     <Navbar/>
     <CartWidget/>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,13 @@ function Navbar(){
   return (
     <header>
       <nav className="nav-bar">
+        <p className="logo">L O G O</p>
         <ul className="nav-bar-items">
         <li>Inicio</li>
         <li>Productos</li>
         <li>Contacto</li>
         </ul>
+  {/* Aca va el CartWidget*/} 
       </nav>
     </header>
   );
