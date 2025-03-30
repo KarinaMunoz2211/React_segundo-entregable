39cdd8502db7a614b71d16792f7b9b23c617e490
Karina Muñoz <karina.m.soto@gmail.com>
2025-03-29 23:10:51 -0300

    change URL Repo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import './ItemListContainer.css';
+import { products } from '../Products';
 
 function ItemListContainer({karySayHi}) {
+
+  useEffect(() => {
+    console.log("lista de productos",products);
+  },[] );
+
   return (
+    <>
     <h1 className="greetings">{karySayHi}</h1>
+    </>
   );
 };
+
 export default ItemListContainer
 
---
+++ src/components/Products.js
@@ -0,0 +1,50 @@
+ export const products = [
+    {
+        id: 1,
+        tittle: "Libreta con Puntos",
+        price: 5000,
+        category: "Cuadernos",
+        stock:20,
+        description: "Libreta de tapa dura con puntos"
+    },
+    {
+        id: 2,
+        tittle: "Pluma Lamy Safari transparente",
+        price: 35000,
+        category: "Escritura",
+        stock:10,
+        description: "Pluma estilográfica con vista transparente"
+    },
+    {
+        id: 3,
+        tittle: "Marcapáginas Perro",
+        price: 2500,
+        category: "Escritorio",
+        stock:25,
+        description: "Marcapágina de Metal con forma de perro salchicha"
+    },
+    {
+        id: 4,
+        tittle: "Cuaderno cuadriculado con flores",
+        price: 5000,
+        category: "Cuadernos",
+        stock:50,
+        description: "Cuaderno con espiral diseño floreado"
+    },
+    {
+        id: 5,
+        tittle: "Set de Notas Adhesivas",
+        price: 5000,
+        category: "Escritorio",
+        stock:10,
+        description: "Set de notas adhesivas de colores"
+    },
+    {
+        id: 6,
+        tittle: "Cartuchos de Tinta Lamy",
+        price: 3500,
+        category: "Escritura",
+        stock:20,
+        description: "Caja de 6 cartuchos de tinta para Lamy Safari"
+    },
+ ];
